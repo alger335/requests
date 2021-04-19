749c512b2740d8a4d851dbb5f3035eff4a2f3aad
Alexey Germanov <alger277@gmail.com>
2021-04-19 15:08:30 +0300

    Task 2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from heroes import *
 
 if __name__ == '__main__':
     smartest_hero(get_heroes(['Hulk', 'Captain America', 'Thanos']))
-    ya = YaUploader(token="AQAAAAAYT1RlAADLW7ZoJ0igKktxuxO5jB4hzWo")
+    ya = YaUploader(token="")
     # ya.upload_file_to_disk("netology", "test.txt")
     ya.upload_file_to_disk("D:\\IT\\Python\\netology\\Full_Course\\requests\\test.txt")
 
